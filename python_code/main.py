ff5981865d607bd3739d1b45a01aef7d01414517
Kei <withwhom0215@gmail.com>
2021-07-08 22:03:20 +0200

    add method to write values to the txt file (not optimal)

--- python_code/main.py
+++ python_code/main.py
@@ -3,7 +3,15 @@ import matplotlib.pyplot as plt
 from scipy.integrate.odepack import odeint
 import params
 from chondrocyte import *
+import os
 
+path = os.getcwd()
+path = os.path.join(path, "result")
+
+if not os.path.isdir(path):
+    print("Create a result folder")
+    os.makedirs(path)
+    
 # Define time span
 t_final = params.t_final
 dt = params.dt
@@ -24,6 +32,86 @@ solution = odeint(rhs, y0, t, args=parameters)
 
 # Split up into individual states
 V, Na_i, K_i, Ca_i, H_i, Cl_i, a_ur, i_ur, vol_i, cal  = np.hsplit(solution, 10)
+
+ramp_Vm = params.ramp_Vm
+if (ramp_Vm == True):
+    V_0 = params.V_0; V_final = params.V_final
+    V = V_0 + (V_final - V_0)*t/t_final
+    #  elseif 
+    #     (clamp_Vm == true)
+    #     global V_0, global V_final, global t_final;
+    #     for
+
+longth = V.shape[0]
+
+
+# get steady-state ion concentrations
+Ca_i_ss = Ca_i[longth-1]
+K_i_ss = K_i[longth-1]
+Na_i_ss = Na_i[longth-1]
+# %Na_i_ss = 12.0
+V_RMP_ss = V[longth-1]
+
+
+ofile8 = os.path.join(path,'IV_K2pore.txt')
+fid8 = open(ofile8,'w')
+ofile9 = os.path.join(path,'voltage.txt')
+fid9 = open(ofile9,'w')
+ofile10 = os.path.join(path,'IV_KATP.txt')
+fid10 = open(ofile10,'w')
+ofile11 = os.path.join(path,'IV_INab.txt')
+fid11 = open(ofile11,'w')
+ofile12 = os.path.join(path,'IV_IKb.txt')
+fid12 = open(ofile12,'w')
+ofile13 = os.path.join(path,'IV_IClb.txt')
+fid13 = open(ofile13,'w')
+ofile14 = os.path.join(path,'IV_INaK.txt')
+fid14 = open(ofile14,'w')
+ofile15 = os.path.join(path,'IV_INaCa.txt')
+fid15 = open(ofile15,'w')
+ofile16 = os.path.join(path,'IV_ICaP.txt')
+fid16 = open(ofile16,'w')
+ofile17 = os.path.join(path,'IV_I_TRPV4.txt')
+fid17 = open(ofile17,'w')
+ofile18 = os.path.join(path,'IV_I_bg.txt')
+fid18 = open(ofile18,'w')
+ofile19 = os.path.join(path,'IV_I_total.txt')
+fid19 = open(ofile19,'w')
+ofile20 = os.path.join(path,'IV_I_K_Ca.txt')
+fid20 = open(ofile20,'w')
+ofile21 = os.path.join(path,'IV_I_K_DR.txt')
+fid21 = open(ofile21,'w')
+ofile22 = os.path.join(path,'IV_act_DR.txt')
+fid22 = open(ofile22,'w')
+ofile23 = os.path.join(path,'RMP_vs_KDR.txt')
+fid23 = open(ofile23,'w')
+ofile24 = os.path.join(path,'IV_TRPV4.txt')
+fid24 = open(ofile24,'w')
+ofile25 = os.path.join(path,'IV_RMP.txt')
+fid25 = open(ofile25,'w')
+
+VV = np.linspace(-150, 100, 2501)
+
+
+fid8.close()
+fid9.close()
+fid10.close()
+fid11.close()
+fid12.close()
+fid13.close()
+fid14.close()
+fid15.close()
+fid16.close()
+fid17.close()
+fid18.close()
+fid19.close()
+fid20.close()
+fid21.close()
+fid22.close()
+fid23.close()
+fid24.close()
+fid25 .close()
+from IPython import embed; embed(); exit(1)
 
 
 
