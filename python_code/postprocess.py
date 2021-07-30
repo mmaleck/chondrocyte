03e7814ba7d9470a1a1c088ce1fb02696dd02c75
Kei <withwhom0215@gmail.com>
2021-07-30 14:11:29 +0200

    updated to minimum working example

--- python_code/postprocess.py
+++ python_code/postprocess.py
@@ -3,13 +3,12 @@ from os import path
 from argparse import ArgumentParser
 import numpy as np
 import matplotlib.pyplot as plt
-import params
+from params import params_dict
 '''
 Postprocess file
-Exmaple usage : python postprocess.py --case="result/2"
+Exmaple usage : python postprocess.py --case="result/1" --
 
-current_name : "alpha_K_DR" 
-               "I_K_DR" 
+current_name : "I_K_DR" 
                "I_NaK" 
                "I_NaCa"
                "I_Ca_ATP"
@@ -25,20 +24,16 @@ current_name : "alpha_K_DR"
                "I_RMP" 
                "I_total"
 
-File name : postprocess.py 
 Author : Kei Yamamoto
 email : keiya@math.uio.no
 Data created : July, 2021
-Data last modified : 
 Python version : 3.8.2
-copyright :
-credits : 
-license : 
 '''
 
 parser = ArgumentParser()
-parser.add_argument('--case', type=str, default="/result/1", help="Path to simulation results",
+parser.add_argument('--case', type=str, default="result/1", help="Path to simulation results",
                         metavar="PATH")
+
 args = parser.parse_args()
 
 current_file = open(path.join(args.case,"current.pkl"), "rb")
@@ -50,19 +45,16 @@ ode_solution = load(ode_solution_file)
 ode_solution_file.close()
 V, Na_i, K_i, Ca_i, H_i, Cl_i, a_ur, i_ur, vol_i, cal  = np.hsplit(ode_solution, 10) 
 
-"""Example for plotting"""
 
-"""I_V plot"""
-V_step_size = 2501
-VV = np.linspace(-150, 100, V_step_size)
+# Example of plotting 
+# I_V plot
+V_step_size = params_dict["V_step_size"]
+VV = np.linspace(params_dict["V_start"], params_dict["V_end"], V_step_size)
 plt.plot(VV, current["I_NaK"])
+plt.xlabel("voltage(mV)")
+plt.ylabel("I,NaK")
+plt.legend()
 plt.show()
-"""time voltage plot"""
-# t_final = params.t_final
-# dt = params.dt
-# t = np.linspace(0, t_final, int(t_final/dt))
-# plt.plot(t, V)
-# plt.show()
 
 """
 You can add codes from here to create plot and play around with the simulation results
